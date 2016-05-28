d95efdd917c10007c06dc8503d874d2b73aaa771
ignacioliveros <ignacioliveros@gmail.com>
2016-05-28 19:45:41 -0300

    Espero que funque

---
+++ IEEETalks.Core/Entities/Event.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEEETalks.Core.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public int EntityId { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Location { get; set; }
+        public int? Quota { get; set; }
+        public DateTime ActivePeriod { get; set; }
+        public float Price { get; set; }
+        public EventState eventState { get; set; }
+        public DateTime EventDate { get; set; }
+    }
+
+    public enum EventState
+    {
+        Active,
+        Suspended,
+        Canceled,
+        Completed,
+        Remove
+    }
+}
---
+++ IEEETalks.Data.Repositories/Repositories/EventRepository/EventRepository.cs
@@ -0,0 +1,18 @@
+﻿using IEEETalks.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEEETalks.Data.Repositories.Repositories.EventRepository
+{
+    public class EventRepository : IBaseRepository<Event>, IEventRepository
+    {
+        public List<Event> GetAllEvents()
+        {
+            var eventList = new List<Event>();
+            return eventList;
+        }
+    }
+}
