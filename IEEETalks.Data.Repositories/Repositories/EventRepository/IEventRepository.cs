6ddb345c4168eac318d0dfb918616b6e1aeec861
ignacioliveros <ignacioliveros@gmail.com>
2016-05-28 19:27:30 -0300

    Este es un test de commit y creación de repository  y class event

---
+++ IEEETalks.Data.Repositories/Repositories/EventRepository/IEventRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using IEEETalks.Core.Entities;
+
+namespace IEEETalks.Data.Repositories.Repositories.EventRepository
+{
+    public interface IEventRepository : IBaseRepository<Event>
+    {
+        List<Event> GetAllEvents();
+    }
+}
---
+++ IEEETalks.Data.Repositories/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEEETalks.Data.Repositories.Repositories
+{
+    public interface IBaseRepository <T> where T : class
+    {
+
+    }
+}
