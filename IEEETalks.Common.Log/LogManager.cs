6253653dc4bf89e83fd8e30a77b1c28cffd06ed9
Christian Escalante <ch.xxii@gmail.com>
2016-07-09 01:55:16 -0300

    fixed tracked files

--- IEEETalks.Common.Log/LogManager.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IEEETalks.Common.Log
-{
-    public static class LogManager
-    {
-        private static ILog _logger;
-        public static ILog Logger => _logger ?? (_logger = new TraceLogger());
-    }
-}
