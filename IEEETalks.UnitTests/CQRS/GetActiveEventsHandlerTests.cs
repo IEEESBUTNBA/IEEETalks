8b1a2e50d0c49d55050f9027ab45f78a23a8f960
franco948 <franco.palermo812@gmail.com>
2016-08-08 16:56:29 -0300

    #63 - CQRS GetActiveEvents

---
+++ IEEETalks.UnitTests/CQRS/GetActiveEventsHandlerTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IEEETalks.Common;
+using IEEETalks.Persistance;
+using Moq;
+using NUnit.Framework;
+using IEEETalks.Core.Entities;
+using IEEETalks.Core.Enums;
+using IEEETalks.CQRS.Queries;
+using NUnit.Framework.Internal.Commands;
+using Shouldly;
+
+namespace IEEETalks.UnitTests.CQRS
+{
+    [TestFixture]
+    class GetActiveEventsHandlerTests
+    {
+        [Test]
+        public void GetActiveEvents_Should_Return_2_Events()
+        { 
+            // Arrange
+            var skip = 2;
+            var limit = 2;
+
+            var myActiveEvents = new List<Event>()
+            {
+                new Event() {Id = Guid.NewGuid()},
+                new Event() {Id = Guid.NewGuid()},
+            };
+
+            var command = new GetActiveEvents(skip, limit);
+
+            var mockSession = new Mock<ISession>();
+
+            mockSession.Setup(x => x.GetQueryable<Event>()).Returns(myActiveEvents.AsQueryable());
+            
+            var handler = new GetActiveEventsHandler(mockSession.Object);
+          
+            // Act
+            var result = handler.Handle(command);
+
+            // Assert
+            mockSession.Verify(x => x.GetQueryable<Event>(), Times.Exactly(2));
+            result.Items.ShouldNotBeNull();
+            
+            var testPass = false;
+            
+            foreach (var @event in result.Items)
+            {
+                foreach (var @activeEvent in myActiveEvents)
+                {
+                    if (@event.Id == @activeEvent.Id)
+                    {
+                            testPass = true;
+                    }
+                }
+
+                if (testPass==false)
+                {
+                    Assert.Fail();
+                }
+
+                testPass = false;
+            }
+        }
+    }
+}
