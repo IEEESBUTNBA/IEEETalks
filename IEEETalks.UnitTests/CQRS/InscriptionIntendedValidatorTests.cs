8db2e325fa6b336c12e21d63da2bbbe85527c066
franco948 <franco.palermo812@gmail.com>
2016-09-03 17:33:23 -0300

    #63 - Changes

---
+++ IEEETalks.UnitTests/CQRS/InscriptionIntendedValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IEEETalks.CQRS.Commands;
+using IEEETalks.Persistance;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace IEEETalks.UnitTests.CQRS
+{
+    class InscriptionIntendedValidatorTests
+    {
+        private InscriptionIntendedValidator inscriptionIntendedValidator;
+
+        [Test]
+        public void ConfigureValidation_Should_Throw_ValidationException_When_FirstName_Is_Empty()
+        {
+            var mockSession = new Mock<ISession>();
+            //mockSession.Setup(x => x.)
+
+            inscriptionIntendedValidator = new InscriptionIntendedValidator(mockSession.Object);
+            //inscriptionIntendedValidator.Validate();
+        }
+    }
+}
--- IEEETalks.UnitTests/CQRS/SaveInscriptionIntendedTests.cs
+++ IEEETalks.UnitTests/CQRS/SaveInscriptionIntendedTests.cs
@@ -10,6 +10,8 @@ using Moq;
 using NUnit.Framework;
 using Shouldly;
 using System.ComponentModel.DataAnnotations;
+using IEEETalks.Common;
+using IEEETalks.Core.Enums;
 
 namespace IEEETalks.UnitTests.CQRS
 {
@@ -44,23 +46,41 @@ namespace IEEETalks.UnitTests.CQRS
         }
 
         [Test]
-        public void SaveInscriptionIntended_Should_Throw_ValidationException()
+        public void SaveInscriptionIntended_Should_Throw_ValidationException_When_FirstName_Is_Empty()
         {
             // Arrange
+            var activeRange = DateTimeRange.CreateOneWeekRange(DateTime.Today.AddDays(-5));
+
+            var myEvent = new Event
+            {
+                Id = new Guid("20133f6d-5356-4ed2-b0fa-75dc73646499"),
+                ActiveSinceDate = activeRange.Start,
+                ActiveUntilDate = activeRange.End,
+                EventState = EventState.Active
+            };
+
+            var mySecondEvent = new Event() { Id = Guid.NewGuid() };
+
+            var myEvents = new List<Event>()
+            {
+                myEvent,
+                mySecondEvent
+            }.AsQueryable();
+
             var myInscription = new InscriptionIntended()
             {
                 FirstName = "",
-                LastName = "",
-                Email = "fergoail.com",
-                EventId = Guid.NewGuid()
+                LastName = "Rodriguez",
+                Email = "fergoail@hotmail.com",
+                EventId = Guid.Parse("20133f6d-5356-4ed2-b0fa-75dc73646499")
 
             };
 
             var command = new SaveInscriptionIntended(myInscription);
             var mockSession = new Mock<ISession>();
-        
+
             mockSession.Setup(x => x.Store(command.InscriptionIntended.Id, command.InscriptionIntended));
-            mockSession.Setup(x => x.GetQueryable<Event>());
+            mockSession.Setup(x => x.GetQueryable<Event>()).Returns(myEvents);
 
             var siiv = new SaveInscriptionIntendedValidator(mockSession.Object);
             var handler = new SaveInscriptionIntendedHandler(mockSession.Object);
@@ -70,15 +90,174 @@ namespace IEEETalks.UnitTests.CQRS
             var validateResult = siiv.Validate(command);
 
             // Assert
-            //Assert.That(() => handler.Handle(command), Throws.TypeOf<ValidationException>());
             mockSession.Verify(x => x.Store(command.InscriptionIntended.Id, command.InscriptionIntended), Times.Once);
             mockSession.Verify(x => x.GetQueryable<Event>(), Times.Once);
 
-            //foreach (var validationFailure in validateResult.Errors)
-            //{
-                
-            //}
-            
+            validateResult.IsValid.ShouldBe(false);
+            validateResult.Errors.Count.ShouldBe(1);
+            validateResult.Errors[0].PropertyName.ShouldBe("FirstName");
+
+            // Testear ambos Validator 
+        }
+
+        [Test]
+        public void SaveInscriptionIntended_Should_Throw_ValidationException_When_LastName_IsEmpty()
+        {
+            // Arrange
+            var activeRange = DateTimeRange.CreateOneWeekRange(DateTime.Today.AddDays(-5));
+
+            var myEvent = new Event
+            {
+                Id = new Guid("20133f6d-5356-4ed2-b0fa-75dc73646499"),
+                ActiveSinceDate = activeRange.Start,
+                ActiveUntilDate = activeRange.End,
+                EventState = EventState.Active
+            };
+
+            var mySecondEvent = new Event() { Id = Guid.NewGuid() };
+
+            var myEvents = new List<Event>()
+            {
+                myEvent,
+                mySecondEvent
+            }.AsQueryable();
+
+            var myInscription = new InscriptionIntended()
+            {
+                FirstName = "Hernan",
+                LastName = "",
+                Email = "fergoail@hotmail.com",
+                EventId = Guid.Parse("20133f6d-5356-4ed2-b0fa-75dc73646499")
+
+            };
+
+            var command = new SaveInscriptionIntended(myInscription);
+            var mockSession = new Mock<ISession>();
+
+            mockSession.Setup(x => x.Store(command.InscriptionIntended.Id, command.InscriptionIntended));
+            mockSession.Setup(x => x.GetQueryable<Event>()).Returns(myEvents);
+
+            var siiv = new SaveInscriptionIntendedValidator(mockSession.Object);
+            var handler = new SaveInscriptionIntendedHandler(mockSession.Object);
+
+            // Act
+            var handleResult = handler.Handle(command);
+            var validateResult = siiv.Validate(command);
+
+            // Assert
+            mockSession.Verify(x => x.Store(command.InscriptionIntended.Id, command.InscriptionIntended), Times.Once);
+            mockSession.Verify(x => x.GetQueryable<Event>(), Times.Once);
+
+            validateResult.IsValid.ShouldBe(false);
+            validateResult.Errors.Count.ShouldBe(1);
+            validateResult.Errors[0].PropertyName.ShouldBe("LastName");
+        }
+
+        [Test]
+        public void SaveInscriptionIntended_Should_Throw_ValidationException_When_Email_IsEmpty()
+        {
+            // Arrange
+            var activeRange = DateTimeRange.CreateOneWeekRange(DateTime.Today.AddDays(-5));
+
+            var myEvent = new Event
+            {
+                Id = new Guid("20133f6d-5356-4ed2-b0fa-75dc73646499"),
+                ActiveSinceDate = activeRange.Start,
+                ActiveUntilDate = activeRange.End,
+                EventState = EventState.Active
+            };
+
+            var mySecondEvent = new Event() { Id = Guid.NewGuid() };
+
+            var myEvents = new List<Event>()
+            {
+                myEvent,
+                mySecondEvent
+            }.AsQueryable();
+
+            var myInscription = new InscriptionIntended()
+            {
+                FirstName = "Hernan",
+                LastName = "Perez",
+                Email = "",
+                EventId = Guid.Parse("20133f6d-5356-4ed2-b0fa-75dc73646499")
+
+            };
+
+            var command = new SaveInscriptionIntended(myInscription);
+            var mockSession = new Mock<ISession>();
+
+            mockSession.Setup(x => x.Store(command.InscriptionIntended.Id, command.InscriptionIntended));
+            mockSession.Setup(x => x.GetQueryable<Event>()).Returns(myEvents);
+
+            var siiv = new SaveInscriptionIntendedValidator(mockSession.Object);
+            var handler = new SaveInscriptionIntendedHandler(mockSession.Object);
+
+            // Act
+            var handleResult = handler.Handle(command);
+            var validateResult = siiv.Validate(command);
+
+            // Assert
+            mockSession.Verify(x => x.Store(command.InscriptionIntended.Id, command.InscriptionIntended), Times.Once);
+            mockSession.Verify(x => x.GetQueryable<Event>(), Times.Once);
+
+            validateResult.IsValid.ShouldBe(false);
+            validateResult.Errors.Count.ShouldBe(2);
+            validateResult.Errors[0].PropertyName.ShouldBe("InscriptionIntended.Email");
+        }
+
+        [Test]
+        public void SaveInscriptionIntended_Should_Throw_ValidationException_When_Email_IsInvalid()
+        {
+            // Arrange
+            var activeRange = DateTimeRange.CreateOneWeekRange(DateTime.Today.AddDays(-5));
+
+            var myEvent = new Event
+            {
+                Id = new Guid("20133f6d-5356-4ed2-b0fa-75dc73646499"),
+                ActiveSinceDate = activeRange.Start,
+                ActiveUntilDate = activeRange.End,
+                EventState = EventState.Active
+            };
+
+            var mySecondEvent = new Event() { Id = Guid.NewGuid() };
+
+            var myEvents = new List<Event>()
+            {
+                myEvent,
+                mySecondEvent
+            }.AsQueryable();
+
+            var myInscription = new InscriptionIntended()
+            {
+                FirstName = "Hernan",
+                LastName = "Gonzales",
+                Email = "fergoailtmail.com",
+                EventId = Guid.Parse("20133f6d-5356-4ed2-b0fa-75dc73646499")
+
+            };
+
+            var command = new SaveInscriptionIntended(myInscription);
+            var mockSession = new Mock<ISession>();
+
+            mockSession.Setup(x => x.Store(command.InscriptionIntended.Id, command.InscriptionIntended));
+            mockSession.Setup(x => x.GetQueryable<Event>()).Returns(myEvents);
+
+            var siiv = new SaveInscriptionIntendedValidator(mockSession.Object);
+            var handler = new SaveInscriptionIntendedHandler(mockSession.Object);
+
+            // Act
+            var handleResult = handler.Handle(command);
+            var validateResult = siiv.Validate(command);
+
+            // Assert
+            mockSession.Verify(x => x.Store(command.InscriptionIntended.Id, command.InscriptionIntended), Times.Once);
+            mockSession.Verify(x => x.GetQueryable<Event>(), Times.Once);
+
+            validateResult.IsValid.ShouldBe(false);
+            validateResult.Errors.Count.ShouldBe(1);
+            validateResult.Errors[0].PropertyName.ShouldBe("InscriptionIntended.Email");
+            validateResult.Errors[0].ErrorMessage.ShouldBe("'Email' no es una dirección de correo electrónico válida.");
         }
     }
 }
